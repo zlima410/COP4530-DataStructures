d0b1a8e4c935852caf840c667a65d76056891a94
zlima <endercraft68@gmail.com>
2024-03-12 10:22:05 -0400

    Begin construction of operator check in buildFromPostfix()

--- assign3/bet.cpp
+++ assign3/bet.cpp
@@ -9,7 +9,7 @@ BET::BET(const string &postfix) : root{nullptr} // one parameter constructor tha
 }
 
 BET::BET(const BET &rhs) : root{nullptr} // copy constructor
-{ 
+{
     ClearStack();
     root = clone(rhs.root);
     betStack.push(root);
@@ -23,12 +23,12 @@ BET::~BET() // destructor
         delete root;
 }
 
-const BET &BET::operator=(const BET &rhs)   // assignment operator overload
+const BET &BET::operator=(const BET &rhs) // assignment operator overload
 {
     if (this != &rhs) // check for self assignment
     {
         if (!empty())
-            makeEmpty(root);    // delete all nodes in the tree if it isn't empty
+            makeEmpty(root); // delete all nodes in the tree if it isn't empty
 
         root = clone(rhs.root); // clone the root
 
@@ -39,7 +39,8 @@ const BET &BET::operator=(const BET &rhs)   // assignment operator overload
     return *this;
 }
 
-bool BET::buildFromPostFix(const string& postfix) {
+bool BET::buildFromPostFix(const string &postfix)
+{
     if (!empty) // make sure a new tree is being made
     {
         makeEmpty(root);
@@ -49,16 +50,27 @@ bool BET::buildFromPostFix(const string& postfix) {
     bool ret = true;
     string post = postfix, token;
 
-    for (string::iterator itr = post.begin(); itr != post.end(); ++itr) {
-        token += *itr;  // add the current character to the token string
+    for (string::iterator itr = post.begin(); itr != post.end(); ++itr)
+    {
+        token += *itr; // add the current character to the token string
 
         if (*itr == ' ' || itr == (post.end() - 1)) // create the tree if the character is the end or a space
         {
-            if (*itr == ' ')    // if the last character is a space remove it
+            if (*itr == ' ') // if the last character is a space remove it
                 token.pop_back();
-            if (isOperand(token)) {
-                BinaryNode *n = new BinaryNode(token, nullptr, nullptr);    // create a new node with the operand
-                betStack.push(n);                                           // push new node into the stack
+            if (isOperand(token))
+            {
+                BinaryNode *n = new BinaryNode(token, nullptr, nullptr); // create a new node with the operand
+                betStack.push(n);                                        // push new node into the stack
+            }
+            else if (isOperator(token)) // if the token is an operator
+            {
+                BinaryNode *rt, *lt; // right and left temporary nodes
+                if (!betStack.empty())
+                {
+                    rt = betStack.top();
+                    betStack.pop();
+                }
             }
         }
     }
